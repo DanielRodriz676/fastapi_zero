21b1de93ca2e8fe0ec9994386a1666e25a00b9c4
daniel Rodriz <daniel.f.rodriz@gmail.com>
2026-09-18 17:33:59 -0300

    feat(auth): implementa refresh token e testes de autorização

--- fastapi_zero/security.py
+++ fastapi_zero/security.py
@@ -4,7 +4,7 @@ from zoneinfo import ZoneInfo
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jwt import DecodeError, decode, encode
+from jwt import DecodeError, ExpiredSignatureError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -15,7 +15,7 @@ from fastapi_zero.settings import Settings
 
 settings = Settings()
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl='auth/token')
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='auth/token', refreshUrl='auth/refresh')
 
 pwd_context = PasswordHash.recommended()
 
@@ -56,6 +56,9 @@ async def get_current_user(
             raise credentials_exception
 
     except DecodeError:
+        raise credentials_exception
+
+    except ExpiredSignatureError:
         raise credentials_exception
 
     user = await session.scalar(select(User).where(User.email == subject_email))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from datetime import datetime
 
+import factory
 import pytest
 import pytest_asyncio
 from fastapi.testclient import TestClient
@@ -12,6 +13,15 @@ from fastapi_zero.app import app
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User, table_registry
 from fastapi_zero.security import get_password_hash
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'test{n}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
 
 
 @pytest.fixture
@@ -67,11 +77,22 @@ def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
 @pytest_asyncio.fixture
 async def user(session):
     password = 'testtest'
-    user = User(
-        username='Teste',
-        email='teste@test.com',
-        password=get_password_hash(password),
-    )
+    user = UserFactory(password=get_password_hash(password))
+
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+
+    user.clean_password = password
+
+    return user
+
+
+@pytest_asyncio.fixture
+async def other_user(session):
+    password = 'testtest'
+    user = UserFactory(password=get_password_hash(password))
+
     session.add(user)
     await session.commit()
     await session.refresh(user)
