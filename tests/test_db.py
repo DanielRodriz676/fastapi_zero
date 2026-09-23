0029b8fa8a05090184d541e507e1400ac709cd47
daniel Rodriz <daniel.f.rodriz@gmail.com>
2026-09-23 06:19:53 -0300

    feat(docker): dockeriza aplicação e adiciona PostgreSQL

--- tests/test_db.py
+++ tests/test_db.py
@@ -19,18 +19,19 @@ async def test_create_user(session, mock_db_time):
 
 
 @pytest.mark.asyncio
-async def test_create_todo(session, user):
-    todo = Todo(
-        title='Test Todo',
-        description='Test Desc',
-        state='draft',
-        user_id=user.id,
-    )
+async def test_create_todo(session, user, mock_db_time):
+    with mock_db_time(model=Todo) as time:
+        todo = Todo(
+            title='Test Todo',
+            description='Test Desc',
+            state='draft',
+            user_id=user.id,
+        )
 
-    session.add(todo)
-    await session.commit()
+        session.add(todo)
+        await session.commit()
 
-    todo = await session.scalar(select(Todo))
+        todo = await session.scalar(select(Todo))
 
     assert asdict(todo) == {
         'description': 'Test Desc',
@@ -38,6 +39,8 @@ async def test_create_todo(session, user):
         'state': 'draft',
         'title': 'Test Todo',
         'user_id': 1,
+        'created_at': time,
+        'updated_at': time,
     }
 
 
--- tests/test_todos.py
+++ tests/test_todos.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import factory.fuzzy
 import pytest
+from sqlalchemy.exc import DataError
 
 from fastapi_zero.models import Todo, TodoState
 
@@ -16,21 +17,25 @@ class TodoFactory(factory.Factory):
     user_id = 1
 
 
-def test_create_todo(client, token):
-    response = client.post(
-        '/todos/',
-        headers={'Authorization': f'Bearer {token}'},
-        json={
-            'title': 'Test todo',
-            'description': 'Test todo description',
-            'state': 'draft',
-        },
-    )
+def test_create_todo(client, token, mock_db_time):
+    with mock_db_time(model=Todo) as time:
+        response = client.post(
+            '/todos/',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'title': 'Test todo',
+                'description': 'Test todo description',
+                'state': 'draft',
+            },
+        )
+
     assert response.json() == {
         'id': 1,
         'title': 'Test todo',
         'description': 'Test todo description',
         'state': 'draft',
+        'created_at': time.isoformat(),
+        'updated_at': time.isoformat(),
     }
 
 
@@ -180,3 +185,58 @@ def test_delete_todo_error(client, token):
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Task not found.'}
+
+
+@pytest.mark.asyncio
+async def test_list_todos_should_return_all_expected_fields(session, client, user, token, mock_db_time):
+    with mock_db_time(model=Todo) as time:
+        todo = TodoFactory.create(user_id=user.id)
+        session.add(todo)
+        await session.commit()
+
+    await session.refresh(todo)
+    response = client.get(
+        '/todos/',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.json()['todos'] == [
+        {
+            'created_at': time.isoformat(),
+            'updated_at': time.isoformat(),
+            'description': todo.description,
+            'id': todo.id,
+            'state': todo.state,
+            'title': todo.title,
+        }
+    ]
+
+
+@pytest.mark.asyncio
+async def test_create_todo_error(session, user):
+    todo = Todo(title='Test Todo', description='Test Desc', state='test', user_id=user.id)
+
+    session.add(todo)
+
+    with pytest.raises(DataError):
+        await session.commit()
+
+
+def test_list_todos_filter_min_length(client, token):
+    tiny_string = 'a'
+    response = client.get(
+        f'/todos/?title={tiny_string}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+def test_list_todos_filter_max_length(client, token):
+    large_string = 'a' * 22
+    response = client.get(
+        f'/todos/?title={large_string}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
